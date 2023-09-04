cfb739a451b6a7ab02937707ec47c305d963784d
0xheynacho <98292399+0xheynacho@users.noreply.github.com>
2023-09-04 12:41:27 -0400

    Update reservaciones.php

--- reservaciones.php
+++ reservaciones.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$conn = new mysqli("localhost","root","","res");
+    $conn = new mysqli("ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","lz433sbvg6yghw2p","ns0u47xfk11p6vh2","cxtc21a6xabzei03");
 //$conn = mysqli_connect("localhost","root","","db1");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
